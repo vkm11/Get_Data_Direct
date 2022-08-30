e8c6a62cab05ea7d5895278cd83ab208e3f2012f
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-08-30 18:27:30 +0530

    Update kyc_report.vue

--- kyc_report.vue
+++ kyc_report.vue
@@ -1,14 +1,18 @@
 <template>
     <vx-card>
-        <h4 class="mb-4">KYC Reports</h4>
+        <h4 class="mb-4">KYC Non Approval Reports</h4>
         <div>
-            <!-- <vs-button>
-                <vue-json-to-csv :json-data=jsondata>Export CSV</vue-json-to-csv>
+            <vs-button class="m-3" style="float:right" :disabled="kycreport == ''">
+                <vue-json-to-csv :json-data=kycreport>Export CSV</vue-json-to-csv>
+            </vs-button>
+            <!-- <vs-button class="m-3" style="float:right" :disabled="kycreport == ''">
+                <vue-json-to-csv :json-data=kycreport>Export PDF</vue-json-to-csv>
             </vs-button> -->
+            
         </div>
 
-        <div class="mb-4">
-            <vs-table stripe search :data="kycreport">
+        <div class="mb-4" ref="content">
+            <vs-table stripe search :data="kycreport" :max-items="this.kycData.per_page" pagination>
                 <template slot="thead"> 
                     <vs-th>No</vs-th> 
                     <vs-th>Agency Name</vs-th> 
@@ -40,25 +44,27 @@
 
 <script>
 import axios from 'axios'; 
+import VueJsonToCsv from 'vue-json-to-csv';
 
 export default {
     name:"KYC-Report",
     components:{
+        VueJsonToCsv
     },
     data(){
         return{
             kycreport: [],
+            kycData: [],
         }
     },
     methods:{
         loadData() {
-            axios.post('http://localhost:8000/api/admin/agent/kycreport')
+            axios.post('/agent/kycreport')
             .then((res) => {
                 this.kycreport = JSON.parse(JSON.stringify(res.data.result.data));
-                //  this.kycreport = res.data.result.data;
-                console.log(this.kycreport)
+                this.kycData = JSON.parse(JSON.stringify(res.data.result));
             });
-        }
+        },
     },
     mounted() {
         this.loadData();
